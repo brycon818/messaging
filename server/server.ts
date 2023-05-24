ba088074400e60bca53065817cd01b773e8474d6
brycon818 <bryan.conejos@gmail.com>
2023-05-24 19:26:12 +0800

    cors4

--- server/server.ts
+++ server/server.ts
@@ -6,8 +6,9 @@ import cors from "@fastify/cors"
 import { userRoutes } from "./routes/users"
 
 const app = fastify()
-app.register(cors, { origin: process.env.CLIENT_URL,
-    methods: ['GET', 'PUT', 'POST']})
+app.register(cors, { origin: false,
+    methods: ['GET', 'PUT', 'POST'],
+    preflight:false})
 app.register(userRoutes)
 
 app.listen({ port: parseInt(process.env.PORT!) })
