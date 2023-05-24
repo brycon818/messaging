7dbe1e269e3f0fd15acf97ff3ff99b073c762b3c
brycon818 <bryan.conejos@gmail.com>
2023-05-24 17:34:34 +0800

    cors2

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -53,10 +53,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   const signup = useMutation({
     mutationFn: (user: User) => {
-      return axios.post(`${import.meta.env.VITE_SERVER_URL}/signup`, user, {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },})
+      return axios.post(`${import.meta.env.VITE_SERVER_URL}/signup`, user)
     },
     onSuccess() {
       navigate("/login")
@@ -66,10 +63,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const login = useMutation({
     mutationFn: (id: string) => {
       return axios
-        .post(`${import.meta.env.VITE_SERVER_URL}/login`, { id }, {
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-          },})
+        .post(`${import.meta.env.VITE_SERVER_URL}/login`, { id })
         .then(res => {
           return res.data as { token: string; user: User }
         })
@@ -82,10 +76,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   const logout = useMutation({
     mutationFn: () => {
-      return axios.post(`${import.meta.env.VITE_SERVER_URL}/logout`, { token }, {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },})
+      return axios.post(`${import.meta.env.VITE_SERVER_URL}/logout`, { token })
     },
     onSuccess() {
       setUser(undefined)
