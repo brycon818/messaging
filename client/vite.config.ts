2e80f9ab944d90b56878c785019796c13be2acde
brycon818 <bryan.conejos@gmail.com>
2023-05-29 08:02:51 +0800

    bcc230529.003

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 
+const root =resolve(__dirname, 'index.html')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'src/pages/Login.tsx'),
-        nested: resolve(__dirname, 'src/pages/LogoutPage.tsx'),
+        main: resolve(root, 'index.html'),
+        login: resolve(__dirname, 'src/pages/Login.tsx'),
+        logout: resolve(__dirname, 'src/pages/LogoutPage.tsx'),
       },
     },
   },
