633589a220206f0d9300383ff066d6cd6b302b0a
brycon818 <bryan.conejos@gmail.com>
2023-05-29 08:21:34 +0800

    bcc230529.006

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
-const root =resolve(__dirname, 'index.html')
+const root =resolve(__dirname, '.')
+const outDir = resolve(__dirname,'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
+  plugins: [reactRefresh()],
   build: {
-    rollupOptions: {
+    rollupOptions: {  
+      outDir,
+      emptyOutDir: true,
       input: {
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'src/pages/Login.tsx'),
